3a585e4c145395e8f92ffac8751edb197961bd7a
cheolminhwang <cheolmin@gmail.com>
2026-04-11 20:48:49 -0600

    Siebel_Migration_Redeploy

---
+++ Siebel/Siebel_Migration_Redeploy/deployMigration.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+while getopts u:p:a:b:d:e:f:g:h: option
+do
+ case "${option}"
+ in
+ u)  USER=${OPTARG};;
+ p)  PASS=${OPTARG};;
+ a)  SAIHOST=${OPTARG};;
+ b)  SAIPORT=${OPTARG};;
+ d)  PROFILE=${OPTARG};;
+ e)  MNAME=${OPTARG};;
+ f)  MDESC=${OPTARG};;
+  esac
+done
+ENCODED="$(echo $USER:$PASS|base64)"
+
+JSON='{
+"DeploymentInfo":
+{
+"PhysicalHostIP":"'$SAIHOST':'$SAIPORT'",
+"ProfileName":"'$PROFILE'",
+"Action":"Deploy"
+},
+"MigrationDeployParams":
+{
+"SiebelMigration":"'$MNAME'",
+"MigrationDesc":"'$MDESC'"
+}
+}'
+
+echo
+echo $JSON
+echo
+
+echo $JSON |  curl -v  -k -H "Content-Length: ${#JSON}"  \
+ -H "Authorization: Basic $ENCODED" \
+ -H "Content-Type: application/json" \
+ -X POST \
+https://$SAIHOST:$SAIPORT/siebel/v1.0/cloudgateway/deployments/migrations/ \
+-d @-
+
